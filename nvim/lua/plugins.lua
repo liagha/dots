e35997e00e698ae0f51c95846697d9d2a82589c7
AliAghaahmadi <itsaliaghaahmadi@gmail.com>
2026-09-06 14:18:23 +0330

    oil: use :vertical modifier instead of removed --vsplit flag

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -53,7 +53,7 @@ require("lazy").setup({
         "stevearc/oil.nvim",
         keys = {
             { "-", "<cmd>Oil<CR>", desc = "Open parent directory" },
-            { "<leader>e", "<cmd>Oil --vsplit<CR>", desc = "Open file tree (vsplit)" },
+            { "<leader>e", "<cmd>vertical Oil<CR>", desc = "Open file tree (vsplit)" },
         },
         config = guarded(function()
             require("oil").setup({
